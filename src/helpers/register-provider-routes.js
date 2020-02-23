da04839a37f140f370fc5e9b848a9791e6834af2
rgwozdz <rich.gwozdz@gmail.com>
2020-02-22 20:06:59 -0800

    Refactor index.

---
+++ src/helpers/register-provider-routes.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const _ = require('lodash')
+
+const composePath = (routePrefix, routeString) => path.posix.join(routePrefix, routeString)
+function registerProviderRoutes ({ provider, controller, server }, options = {}) {
+  const { routePrefix = '' } = options
+  const { routes = [], namespace } = provider
+  const providerRoutes = {}
+
+  routes.forEach(route => {
+    const { path, methods } = route
+    const composedPath = composePath(routePrefix, path)
+    registerRoutes({
+      server,
+      path: composedPath,
+      methods,
+      controller: bindController({ controller, route, namespace})
+    })
+    providerRoutes[composedPath] = methods
+  })
+
+  return providerRoutes
+}
+
+function bindController (params) {
+  const { controller, route: { handler, path }, namespace } = params
+
+  if (!controller[handler]) throw new Error (`Handler "${handler}" assigned to route "${path}" by the "${namespace}" provider is undefined for the Koop controller`)
+  return controller[handler].bind(controller)
+}
+
+function registerRoutes (params) {
+  const {
+    server,
+    path,
+    methods,
+    controller,
+  } = params
+
+  methods.forEach(method => {
+      server[method](path, controller)
+  })
+}
+
+module.exports = registerProviderRoutes
---
+++ test/helpers/register-provider-routes.spec.js
@@ -0,0 +1,66 @@
+const should = require('should') // eslint-disable-line
+const sinon = require('sinon')
+require('should-sinon')
+const _ = require('lodash')
+const registerProviderRoutes = require('../../src/helpers/register-provider-routes')
+
+const mockController = {
+  testHandler: () => {}
+}
+
+describe('Tests for register-provider-routes', function () {
+
+  it('should register a provider route', () => {
+    const mockServer = sinon.spy({
+      get: () => {}
+    })
+
+    const mockProvider = {
+      namespace: 'mock-provider',
+      hosts: true,
+      disableIdParam: false,
+      routes: [{
+        path: '/test/route',
+        methods: ['get'],
+        handler: 'testHandler'
+      }]
+    }
+    const providerRouteMap = registerProviderRoutes({
+      provider: mockProvider,
+      controller: mockController,
+      server: mockServer
+    })
+  
+    mockServer.get.should.be.calledOnce()
+    providerRouteMap.should.deepEqual({ "/test/route": [ "get" ] })
+  })
+
+  it('should throw an error if handler not found on Koop controller', () => {
+    const mockServer = sinon.spy({
+      get: () => {}
+    })
+
+    const mockProvider = {
+      namespace: 'mock-provider',
+      hosts: true,
+      disableIdParam: false,
+      routes: [{
+        path: '/test/route',
+        methods: ['get'],
+        handler: 'testHandler'
+      }]
+    }
+
+    try {
+      registerProviderRoutes({
+        provider: mockProvider,
+        controller: {},
+        server: mockServer
+      })
+      should.fail('Should have thrown')
+    } catch (err) {
+      mockServer.get.should.be.callCount(0)
+      err.message.should.equal(`Handler "testHandler" assigned to route "/test/route" by the "mock-provider" provider is undefined for the Koop controller`)
+    }
+  })
+})
