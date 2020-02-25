6918771bfd8a30ee5dd981fbe4cd14dda5b1394b
Haoliang Yu <haoliang.yu@outlook.com>
2020-02-25 08:43:37 -0500

    fix test failure

--- test/model-test.js
+++ test/model-test.js
@@ -30,7 +30,7 @@ describe('Tests for models/index', function () {
 
       retrieveSpy.should.be.calledOnce()
       retrieveSpy.firstCall.args.length.should.equal(3)
-      retrieveSpy.firstCall.args[0].should.equal('test-provider')
+      retrieveSpy.firstCall.args[0].should.equal('test-provider::data')
       retrieveSpy.firstCall.args[1].should.be.an.Object().and.be.empty()
       retrieveSpy.firstCall.args[2].should.be.an.Function()
       pullSpy.should.be.calledOnce()
@@ -55,7 +55,7 @@ describe('Tests for models/index', function () {
 
       retrieveSpy.should.be.calledOnce()
       retrieveSpy.firstCall.args.length.should.equal(3)
-      retrieveSpy.firstCall.args[0].should.equal('test-provider::host-param::id-param::layer-param')
+      retrieveSpy.firstCall.args[0].should.equal('test-provider::host-param::id-param::layer-param::data')
       retrieveSpy.firstCall.args[1].should.be.an.Object().and.be.empty()
       retrieveSpy.firstCall.args[2].should.be.an.Function()
       pullSpy.should.be.calledOnce()
@@ -80,7 +80,7 @@ describe('Tests for models/index', function () {
       model.pull({ url: 'domain/test-provider', query: {} }, pullSpy)
       retrieveSpy.should.be.calledOnce()
       retrieveSpy.firstCall.args.length.should.equal(3)
-      retrieveSpy.firstCall.args[0].should.equal('custom-key')
+      retrieveSpy.firstCall.args[0].should.equal('custom-key::data')
       retrieveSpy.firstCall.args[1].should.be.an.Object().and.be.empty()
       retrieveSpy.firstCall.args[2].should.be.an.Function()
       pullSpy.should.be.calledOnce()
