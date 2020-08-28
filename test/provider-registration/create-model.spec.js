9efd5605d0cbd38c8f64dc21f83922a20461361c
Rich <rich.gwozdz@gmail.com>
2020-08-28 09:58:39 -0700

    Refactor Model.pull to use await/async

--- test/provider-registration/create-model.spec.js
+++ test/provider-registration/create-model.spec.js
@@ -1,6 +1,7 @@
 /* eslint handle-callback-err: "off" */
 const should = require('should') // eslint-disable-line
 const sinon = require('sinon')
+const { promisify} = require('util')
 require('should-sinon')
 const _ = require('lodash')
 const providerMock = require('../fixtures/fake-provider')
@@ -9,30 +10,33 @@ const koopMock = { test: 'value' }
 
 describe('Tests for create-model', function () {
   describe('createKey', function () {
-    it('should create cache-key as provider name', () => {
+    it.only('should create cache-key as provider name', (done) => {
       const retrieveSpy = sinon.spy(function (key, queryParams, callback) {
         callback(null, {})
       })
-      const pullSpy = sinon.spy()
+      const pullSpy = sinon.spy(() => {
+        retrieveSpy.should.be.calledOnce()
+        retrieveSpy.firstCall.args.length.should.equal(3)
+        retrieveSpy.firstCall.args[0].should.equal('test-provider::data')
+        retrieveSpy.firstCall.args[1].should.be.an.Object().and.be.empty()
+        retrieveSpy.firstCall.args[2].should.be.an.Function()
+        pullSpy.should.be.calledOnce()
+        pullSpy.firstCall.args.length.should.equal(2)
+        pullSpy.firstCall.args.should.deepEqual([null, {}])
+        done()
+      })
 
       // create a model with mocked cache "retrieve" function
       const model = createModel({ ProviderModel: providerMock.Model, koop: koopMock }, {
         cache: {
-          retrieve: retrieveSpy
+          retrieve: retrieveSpy,
+          upsert: () => {}
         }
       })
 
       model.pull({ url: 'domain/test-provider', params: {}, query: {} }, pullSpy)
 
-      retrieveSpy.should.be.calledOnce()
-      retrieveSpy.firstCall.args.length.should.equal(3)
-      retrieveSpy.firstCall.args[0].should.equal('test-provider::data')
-      retrieveSpy.firstCall.args[1].should.be.an.Object().and.be.empty()
-      retrieveSpy.firstCall.args[2].should.be.an.Function()
-      pullSpy.should.be.calledOnce()
-      pullSpy.firstCall.args.length.should.equal(2)
-      should.not.exist(pullSpy.firstCall.args[0])
-      pullSpy.firstCall.args[1].should.be.an.Object().and.be.empty()
+
     })
 
     it('should create cache-key as provider name and concenated url params', () => {
